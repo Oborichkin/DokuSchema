e2f1a7c652d3de7b0fb0403e0b019ea58be0b8cd
Pavel Oborin <oborin.p@gmail.com>
2020-05-04 22:09:22 +0300

    ✨ Working with tables

--- dokuschema/dokuwiki.py
+++ dokuschema/dokuwiki.py
@@ -1,20 +1,31 @@
 from abc import ABC, abstractmethod
+from typing import Iterable, List, Tuple
 
 
 class PageElementFactory(ABC):
     @abstractmethod
-    def heading(self, text, level):
+    def heading(self, text: str, level: int):
         pass
 
     @abstractmethod
-    def text(self, text):
+    def text(self, text: str):
+        pass
+
+    @abstractmethod
+    def table(self, rows: List[Tuple], heading: Iterable):
         pass
 
 
 class DokuWikiFactory(PageElementFactory):
-    def heading(self, text, level=1):
+    def heading(self, text: str, level: int = 1):
         assert 1 <= level <= 5, "DokuWiki supports heading level between 1 and 5"
         return f"{'='*(7-level)} {text} {'='*(7-level)}\n\n"
 
-    def text(self, text):
+    def text(self, text: str):
         return f"{text}\n\n"
+
+    def table(self, rows: List[Tuple], heading: Iterable):
+        res = [f"^ {' ^ '.join(heading)} ^"]
+        for row in rows:
+            res.append(f"| {' | '.join(row)} |")
+        return "\n".join(res)
--- dokuschema/main.py
+++ dokuschema/main.py
@@ -14,16 +14,29 @@ class DokuSchema:
 
 
 class Element:
-    def __init__(self, json_obj: dict):
-        self.title = json_obj.get("title")
+    def __init__(self, json_obj: dict, name: str = None):
+        self.title = json_obj.get("title", "NO TITLE")
+        self.name = self.title if name is None else name
         self.desc = json_obj.get("description")
-        self.properties = [Element(json_obj["properties"][prop]) for prop in json_obj.get("properties", [])]
+        self.type = json_obj["type"]
+        self.properties = []
+        for name, prop in json_obj.get("properties", {}).items():
+            if "allOf" not in prop and "$ref" not in prop:
+                self.properties.append(Element(prop, name))
         self.factory = DokuWikiFactory()
+
+    @property
+    def reference(self):
+        heading = ("Имя", "Тип")
+        rows = [(prop.name, prop.type) for prop in self.properties]
+        return [heading, *rows]
 
     def doku(self, heading_depth=1):
         if self.title:
             res = self.factory.heading(self.title, heading_depth)
             res += self.factory.text(self.desc)
+            res += self.factory.table(heading=self.reference[0], rows=self.reference[1:])
+            res += "\n"
             for prop in self.properties:
                 res += prop.doku(heading_depth + 1)
             return res
